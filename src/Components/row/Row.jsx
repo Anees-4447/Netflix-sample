23db3bb126175181040d703d7d5ad746f35f3d39
Anees <Aneespengad4447@gmail.com>
2024-12-16 11:53:07 +0530

    row section almsot completed

--- src/Components/row/Row.jsx
+++ src/Components/row/Row.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import './Row.css'
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import instance from "../../instence";
+import { imageUrl } from "../../constants";
 
-function Row() {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  const fetchData = async () => {
+    try {
+      const { data } = await instance.get(fetchUrl);
+      console.log(data.results);
+      setMovies(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="row">
-        <h1 className="title">Netflix Originals</h1>
+      <h1 className="title">{title}</h1>
+      {movies?.map((items) => (
         <div className="posters">
-            <img src="/images/money-heist-cast-poster.jpg" alt="" />
+          <img src={`${imageUrl}/${items.backdrop_path}`} alt={items.title || ""} />
         </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
