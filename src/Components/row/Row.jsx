50d76223ffb188b4e8aab8671c6e4b4a63e06e97
Anees <Aneespengad4447@gmail.com>
2024-12-17 14:05:30 +0530

    final commit

--- src/Components/row/Row.jsx
+++ src/Components/row/Row.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import instance from "../../instence";
+import YouTube from "react-youtube";
 import { imageUrl } from "../../constants";
+import {API_KEY} from '../../constants'
+import axios from "axios";
 
 function Row({ title, fetchUrl, isSmall }) {
   const [movies, setMovies] = useState([]);
+  const [video,setVideo] = useState(null)
   const fetchData = async () => {
     try {
       const { data } = await instance.get(fetchUrl);
@@ -17,14 +21,39 @@ function Row({ title, fetchUrl, isSmall }) {
   useEffect(() => {
     fetchData();
   }, []);
+
+  async function movieHandle (id){
+    try{
+     const response = await instance.get(`movie/${id}/videos?api_key=${API_KEY}`)
+        if(response.data.results.lenght !== 0){
+          setVideo(response.data.results[0])
+        }else{
+          console.log("Video not available...!!")
+        }
+      }catch(error){
+      console.log(error)
+    }
+
+  }
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  
   return (
     <div className="row">
       <h1 className="title">{title}</h1>
       <div className="posters">
       {movies?.map((items) => (
-          <img className={ isSmall? 'smallPoster_img' :'posters_img'}  src={`${imageUrl}/${items.backdrop_path}`} alt={items.title || ""} />
+          <img  onClick={(() => movieHandle(items.id))} className={ isSmall? 'smallPoster_img' :'posters_img'}  src={`${imageUrl}/${items.backdrop_path}`} alt={items.title || ""} />
         ))}
         </div>
+       { video && <YouTube videoId={video.key} opts={opts}  />  }
     </div>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-// export const API_KEY = "ec92ae3a154647697f4aad56d706edd9";
+export const API_KEY = "ec92ae3a154647697f4aad56d706edd9";
 export const imageUrl = "https://image.tmdb.org/t/p/original/"
+
